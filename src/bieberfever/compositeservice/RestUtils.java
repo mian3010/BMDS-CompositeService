b371aa2e2e750640ab140e02e6a2fead0f128103
Niclas Tollstorff <niclastollstorff@gmail.com>
2012-09-24 14:08:24 +0200

    added util-class for REST communication

---
+++ src/bieberfever/compositeservice/RestUtils.java
@@ -0,0 +1,87 @@
+package bieberfever.compositeservice;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * 
+ * @author BieberFever
+ */
+public class RestUtils {
+
+	/**
+	 * Do a REST call to a specific URI
+	 * @param requestUri The URI to connect to
+	 * @param httpMethod The HTTP request type to use (POST, GET etc.)
+	 * @param postdata The data to post if the request type is POST
+	 * @return The response for the request
+	 */
+	public String DoRestCall(String requestUri, String httpMethod, String postdata) {
+
+		String response = "";
+		try {
+			// connect to the web service via HTTP GET
+			URL url = new URL(requestUri);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			
+			if (connection.getResponseCode() != 200) {
+			    throw new IOException(connection.getResponseMessage());
+			}
+
+			connection.setRequestMethod(httpMethod);
+
+			if ("POST".equals(httpMethod)) {
+				
+				//This connection is used for output
+				connection.setDoOutput(true);
+
+				//set the content type
+				connection.setRequestProperty("Content-Type", "application/xml");
+				
+				OutputStreamWriter outputStream = new OutputStreamWriter(connection.getOutputStream());
+				outputStream.write(postdata);
+				outputStream.close();
+			}
+			
+			//connect, receive response, then disconnect
+			connection.connect();
+			response = ReceiveResponse(connection);
+			
+			connection.disconnect();
+
+		} catch (IOException e) {
+			System.err.println(e.toString());
+		}
+		return response;
+	}
+
+	/**
+	 * Receives a response from a connection
+	 * @param connection The connection to receive a result from
+	 * @return The response
+	 * @throws IOException Throws an exception with the server response message if the response cannot be read
+	 */
+	private String ReceiveResponse(HttpURLConnection connection) throws IOException {
+		String response = "";
+		try {
+			InputStream inputStream = connection.getInputStream();
+			// read the contents
+			byte[] buffer = new byte[1024];
+			int length;
+			//while there's more input to read (restricted to 1024 bytes)
+			while ((length = inputStream.read(buffer)) > 0) {
+				for (int i = 0; i < length; i++) {
+					response += (char) buffer[i];
+				}
+			}
+			inputStream.close();
+		} catch (IOException e) {
+			throw new IOException(connection.getResponseMessage());
+		}
+		return response;
+	}
+}
