543c6b3fd3d1cc21e2c417dac2fb499e669dc0e9
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-09-24 13:19:35 +0200

    First method

--- src/bieberfever/compositeservice/CompositeService.java
+++ src/bieberfever/compositeservice/CompositeService.java
@@ -2,6 +2,26 @@ package bieberfever.compositeservice;
 
 public class CompositeService {
 
+	public static String getAttendantTasks(String attendantId, int options) {
+		
+	}
 	
+	/**
+	 * Niclas
+	 * @param attendantId
+	 * @return
+	 */
+	private static String getRestAttendantTasks(String attendantId) {
+		
+	}
+	
+	/**
+	 * Michael
+	 * @param attendantId
+	 * @return
+	 */
+	private static String getSoapAttendantTasks(String attendantId) {
+		
+	}
 
 }
