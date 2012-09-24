0a06ff24f1361beb322e12338cfaa97fee240767
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-09-24 13:22:46 +0200

    All methods lol

--- src/bieberfever/compositeservice/CompositeService.java
+++ src/bieberfever/compositeservice/CompositeService.java
@@ -2,6 +2,12 @@ package bieberfever.compositeservice;
 
 public class CompositeService {
 
+	/**
+	 * Niels
+	 * @param attendantId
+	 * @param options
+	 * @return
+	 */
 	public static String getAttendantTasks(String attendantId, int options) {
 		
 	}
@@ -21,6 +27,56 @@ public class CompositeService {
 	 * @return
 	 */
 	private static String getSoapAttendantTasks(String attendantId) {
+		
+	}
+	
+	/**
+	 * Niels
+	 * @param taskXml
+	 * @param options
+	 */
+	public static void createTask(String taskXml, int options) {
+		
+	}
+	
+	/**
+	 * Niclas
+	 * @param taskXml
+	 */
+	private static void createRestTask(String taskXml) {
+		
+	}
+	
+	/**
+	 * Michael
+	 * @param taskXml
+	 */
+	private static void createSoapTask(String taskXml) {
+		
+	}
+	
+	/**
+	 * Niels
+	 * @param task_id
+	 * @param option
+	 */
+	public static void deleteTask(String task_id, int option) {
+		
+	}
+	
+	/**
+	 * Niclas
+	 * @param task_id
+	 */
+	private static void deleteRestTask(String task_id) {
+		
+	}
+	
+	/**
+	 * Michael
+	 * @param task_id
+	 */
+	private static void deleteSoapTask(String task_id) {
 		
 	}
 
