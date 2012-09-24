a93e8d13163c31c59f4b4cd95f3d27ed25381fdd
Claus L. Henriksen <clih@itu.dk>
2012-09-24 13:33:15 +0200

    Added JUnit and Util

---
+++ src/bieberfever/compositeservice/CompositeServiceTest.java
@@ -0,0 +1,24 @@
+package bieberfever.compositeservice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompositeServiceTest {
+
+  @Test
+  public void testGetAttendantTasks() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCreateTask() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testDeleteTask() {
+    fail("Not yet implemented");
+  }
+
+}
